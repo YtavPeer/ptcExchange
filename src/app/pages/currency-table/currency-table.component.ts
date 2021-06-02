ea66425538ccf46f7067f60423a4e3aaa46d043b
YtavPeer <ytav.peer@gmail.com>
2021-06-02 18:46:44 +0300

    update table

--- src/app/pages/currency-table/currency-table.component.ts
+++ src/app/pages/currency-table/currency-table.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface Transaction {
+  item: string;
+  cost: number;
+}
 
 @Component({
   selector: 'app-currency-table',
   templateUrl: './currency-table.component.html',
   styleUrls: ['./currency-table.component.scss']
 })
-export class CurrencyTableComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CurrencyTableComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  displayedColumns: string[] = ['item', 'cost'];
+  transactions: Transaction[] = [
+    {item: 'Beach ball', cost: 4},
+    {item: 'Towel', cost: 5},
+    {item: 'Frisbee', cost: 2},
+    {item: 'Sunscreen', cost: 4},
+    {item: 'Cooler', cost: 25},
+    {item: 'Swim suit', cost: 15},
+  ];
+
+  /** Gets the total cost of all transactions. */
+  getTotalCost() {
+    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+  }
 }
